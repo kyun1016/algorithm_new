731192a39693a8aad85648115957329c6de083b6
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2024-10-26 15:26:07 +0900

    이분탐색

--- Algorithm/Algorithm/BinarySearch_Q2143.h
+++ Algorithm/Algorithm/BinarySearch_Q2143.h
@@ -1,3 +1,4 @@
+#pragma once
 #define OUT
 
 #include <queue>
@@ -147,11 +148,11 @@ private:
 /*--------------------
 * main
 --------------------*/
-int main()
-{
-	unique_ptr<QBase> q = make_unique<Q2143>();
-	q->Init();
-	q->Solve();
-
-	return 0;
-}
+//int main()
+//{
+//	unique_ptr<QBase> q = make_unique<Q2143>();
+//	q->Init();
+//	q->Solve();
+//
+//	return 0;
+//}
